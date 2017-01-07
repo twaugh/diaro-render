6d1c833fb186aec0bcdfe0c8dfc3b93289e07b08
Tim Waugh <twaugh@redhat.com>
2017-01-07 16:10:33 +0000

    read locations

--- diaro_render/data.py
+++ diaro_render/data.py
@@ -4,15 +4,80 @@ Read Diaro data format
 
 from xml.etree import ElementTree as ET
 from collections import namedtuple
+import logging
 
 
-DiaroFolder = namedtuple('DiaroFolder', ['title', 'color', 'pattern'])
+DIARO_FOLDER_PROPS = ['title', 'color', 'pattern']
+DiaroFolder = namedtuple('DiaroFolder', DIARO_FOLDER_PROPS)
+
+DIARO_LOCATION_PROPS = ['title', 'address', 'lat', 'lng', 'zoom']
+DiaroLocation = namedtuple('DiaroLocation', DIARO_LOCATION_PROPS)
 
 
 class Diaro(object):
     def __init__(self, filename):
         self.folders = {}  # uid -> DiaroFolder
+        self.locations = {}  # uid -> DiaroLocation
 
         root = ET.parse(filename).getroot()
+        self._parse_root(root)
+
+    def _gather_properties(self, node, properties_without_uid):
+        properties = {
+            'uid': None,
+        }
+
+        for prop in properties_without_uid:
+            properties[prop] = None
+
+        for prop in node:
+            if properties[prop.tag] is None:
+                properties[prop.tag] = prop.text or ''
+            else:
+                logging.warning("property %s defined twice for node %s",
+                                node.tag)
+
+        return properties
+
+    def _parse_folders(self, folders):
+        for folder in folders:
+            assert folder.tag == 'r'
+            properties = self._gather_properties(folder, DIARO_FOLDER_PROPS)
+            if None in properties.values():
+                logging.error("incomplete property list for folder %s",
+                              properties.get('uid', '(?)'))
+            else:
+                uid = properties.pop('uid')
+                diaro_folder = DiaroFolder(**properties)
+                self.folders[uid] = diaro_folder
+                logging.info("folder: %s", uid)
+
+    def _parse_locations(self, locations):
+        for location in locations:
+            assert location.tag == 'r'
+            properties = self._gather_properties(location, DIARO_LOCATION_PROPS)
+            if None in properties.values():
+                logging.error("incomplete property list for location %s",
+                              properties.get('uid', '(?)'))
+                logging.error("properties: %r", properties)
+            else:
+                uid = properties.pop('uid')
+                diaro_location = DiaroLocation(**properties)
+                self.locations[uid] = diaro_location
+                logging.info("location: %s", uid)
+
+    def _parse_root(self, root):
         assert root.tag == 'data'
         assert root.attrib['version'] == '2'
+
+        for child in root:
+            if child.tag == 'table':
+                name = child.attrib['name']
+                if name == 'diaro_folders':
+                    self._parse_folders(child)
+                elif name == 'diaro_locations':
+                    self._parse_locations(child)
+                else:
+                    raise NotImplementedError
+            else:
+                raise NotImplementedError
--- tests/test_data.py
+++ tests/test_data.py
@@ -14,4 +14,105 @@ class TestDiaro(object):
         with NamedTemporaryFile(mode='w') as fp:
             fp.write(xml)
             fp.flush()
-            d = Diaro(filename=fp.name)
+            diaro = Diaro(filename=fp.name)
+
+    def test_folder(self):
+        xml = dedent("""\
+            <data version="2">
+            <table name="diaro_folders">
+            <r>
+               <uid>73c5b749a50f0628667988147ca663c2</uid>
+               <title>Diary entries</title>
+               <color>#000000</color>
+               <pattern>pattern01</pattern>
+            </r>
+            </table>
+            </data>
+            """)
+
+        with NamedTemporaryFile(mode='w') as fp:
+            fp.write(xml)
+            fp.flush()
+            diaro = Diaro(filename=fp.name)
+
+        assert len(diaro.folders) == 1
+        assert '73c5b749a50f0628667988147ca663c2' in diaro.folders
+        folder = diaro.folders['73c5b749a50f0628667988147ca663c2']
+        assert folder.title == 'Diary entries'
+        assert folder.color == '#000000'
+        assert folder.pattern == 'pattern01'
+
+    @pytest.mark.parametrize('folders', [
+        [
+            {
+                'uid': '1',
+                'title': 'title1',
+                'color': '#111111',
+                'pattern': 'patern01',
+            },
+
+            {
+                'uid': '2',
+                'title': 'title2',
+                'color': '#222222',
+                'pattern': 'pattern02',
+            },
+        ],
+    ])
+    def test_folders(self, folders):
+        xml = dedent("""\
+            <data version="2">
+            <table name="diaro_folders">
+            """)
+
+        for folder in folders:
+            xml += dedent("""\
+                <r>
+                  <uid>{uid}</uid>
+                  <title>{title}</title>
+                  <color>{color}</color>
+                  <pattern>{pattern}</pattern>
+                </r>
+                """.format(**folder))
+
+        xml += dedent("""\
+            </table>
+            </data>
+            """)
+
+        with NamedTemporaryFile(mode='w') as fp:
+            fp.write(xml)
+            fp.flush()
+            diaro = Diaro(filename=fp.name)
+
+        assert len(diaro.folders) == len(folders)
+
+    def test_location(self):
+        xml = dedent("""\
+            <data version="2">
+            <table name="diaro_locations">
+            <r>
+               <uid>1</uid>
+               <title></title>
+               <address>1 Two Road, New Old Street</address>
+               <lat>50.000000</lat>
+               <lng>-1.600000</lng>
+               <zoom>5</zoom>
+            </r>
+            </table>
+            </data>
+            """)
+
+        with NamedTemporaryFile(mode='w') as fp:
+            fp.write(xml)
+            fp.flush()
+            diaro = Diaro(filename=fp.name)
+
+        assert len(diaro.locations) == 1
+        assert '1' in diaro.locations
+        location = diaro.locations['1']
+        assert location.title == ''
+        assert location.address == '1 Two Road, New Old Street'
+        assert location.lat == '50.000000'
+        assert location.lng == '-1.600000'
+        assert location.zoom == '5'
